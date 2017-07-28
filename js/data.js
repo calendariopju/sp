8919bf69f4cafe8381f7767a9bc0cc9f026c9851
Thiago Luis <thiago.jesus@cenpec.org.br>
2017-07-28 16:10:15 -0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -50,12 +50,12 @@ var codropsEvents = {
 	'08-03-2017' : '<span>4ª semana de oficina e encontro</span>',
 	'08-04-2017' : '<span>4ª semana de oficina e encontro</span>',
 	'08-08-2017' : '<span>4ª Formação Continuada</span>',
-	'08-22-2017' : '<span>Formação Temática: Letramento</span>'
+	'08-22-2017' : '<span>Formação Temática: Letramento</span>',
 	'08-28-2017' : '<span>Módulo Projeto</span>',
 	'09-01-2017' : '<span>Encontro Público Cidade Tiradentes e São Miguel Paulista</span>',
 	'09-05-2017' : '<span>5ª Formação Continuada</span>',
 	'09-07-2017' : '<span>Independência do Brasil</span>',
-	'08-22-2017' : '<span>Formação Temática: Étnico Racial</span>'
+	'08-22-2017' : '<span>Formação Temática: Étnico Racial</span>',
 	'10-03-2017' : '<span>6ª Formação Continuada</span>',
 	'10-12-2017' : '<span>Dia de Nossa Sra. Aparecida</span>',
 	'10-18-2017' : '<span>Banca de Projetos</span>',
